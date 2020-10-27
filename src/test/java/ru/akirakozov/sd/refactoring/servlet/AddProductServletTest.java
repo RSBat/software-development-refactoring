4e9f4e6a04e1958c546d9101a15f5a3d295d541b
Sergei Kozelko <rsbat@yandex-team.ru>
2020-10-27 06:09:11 +0300

    Add DB interaction test

--- src/test/java/ru/akirakozov/sd/refactoring/servlet/AddProductServletTest.java
+++ src/test/java/ru/akirakozov/sd/refactoring/servlet/AddProductServletTest.java
@@ -1,6 +1,12 @@
 package ru.akirakozov.sd.refactoring.servlet;
 
+import org.eclipse.jetty.util.IO;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockedStatic;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,10 +14,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(DriverManager.class)
 class AddProductServletTest {
     private final AddProductServlet servlet = new AddProductServlet();
     private final StringWriter writer = new StringWriter();
@@ -66,6 +78,42 @@ class AddProductServletTest {
         verify(response, never()).setStatus(anyInt());
     }
 
+    @Test
+    public void whenQueryIsSuccessfulThenSqlQueryIsMade() throws IOException, SQLException {
+        HttpServletRequest request = createRequestMock("test", "0");
+        HttpServletResponse response = createResponseMock();
+
+        Connection connection = mock(Connection.class);
+        Statement statement = mock(Statement.class);
+        MockedStatic<DriverManager> driverManager = mockStatic(DriverManager.class);
+
+        when(DriverManager.getConnection(any())).thenReturn(connection);
+        when(connection.createStatement()).thenReturn(statement);
+
+        servlet.doGet(request, response);
+
+        verify(statement).executeUpdate(createSqlQuery("test", 0));
+
+        driverManager.close();
+    }
+
+    @Test
+    public void whenSqlQueryFailsExceptionIsRethrown() throws IOException, SQLException {
+        HttpServletRequest request = createRequestMock("test", "0");
+        HttpServletResponse response = createResponseMock();
+
+        Connection connection = mock(Connection.class);
+        MockedStatic<DriverManager> driverManager = mockStatic(DriverManager.class);
+
+        when(DriverManager.getConnection(any())).thenReturn(connection);
+        when(connection.createStatement()).thenThrow(new RuntimeException());
+
+        // TODO check cause
+        assertThrows(RuntimeException.class, () -> servlet.doGet(request, response));
+
+        driverManager.close();
+    }
+
     private HttpServletRequest createRequestMock(String name, String price) {
         HttpServletRequest mock = mock(HttpServletRequest.class);
         when(mock.getParameter("name")).thenReturn(name);
@@ -79,5 +127,10 @@ class AddProductServletTest {
         when(mock.getWriter()).thenReturn(new PrintWriter(writer));
 
         return mock;
+    }
+
+    private String createSqlQuery(String name, long price) {
+        return "INSERT INTO PRODUCT " +
+                "(NAME, PRICE) VALUES (\"" + name + "\"," + price + ")";
     }
 }
